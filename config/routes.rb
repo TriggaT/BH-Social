212d7be41ec0bc570fc1b0ce23ca88f90e7c21fe
Trayshawn <traytwebb14@yahoo.com>
2021-02-01 12:12:40 -0600

    topics model added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
 
+  resources :topics
   devise_for :users, :controllers => { registrations: 'registrations' }
   
 
--- db/migrate/20210201073644_devise_create_users.rb
+++ db/migrate/20210201073644_devise_create_users.rb
@@ -4,8 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :first_name,               null: false, default: "" 
-      t.string :first_name,               null: false, default: "" 
+      t.string :first_name,         null: false, default: "" 
+      t.string :last_name,          null: false, default: "" 
+      t.string :username,           null: false, default: "" 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
